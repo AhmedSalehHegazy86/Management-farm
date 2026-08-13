16942b8d008cfd8c5a6eb53356810ef72f702048
AhmedSalehHegazy86 <ahmedhegazyy86@gmail.com>
2026-08-13 03:31:27 +0300

    app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import streamlit.components.v1 as components
 # ---------------------------------------------------------
 # 1. إعدادات الصفحة وتصميم الخلفية الزرقاء المتدرجة الداكنة
 # ---------------------------------------------------------
-st.set_page_config()
-    page_title="Broiler Farm Manager - Secure Auth",
+st.set_page_config(
+    page_title="Broiler Farm Manager - Secure Auth",)
     page_icon="🐔",
     layout="wide",
     initial_sidebar_state="collapsed"
