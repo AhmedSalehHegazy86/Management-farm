6629ff6ff678fe422ebe6eb2e0e151ae22dff62b
AhmedSalehHegazy86 <ahmedhegazyy86@gmail.com>
2026-08-13 04:43:46 +0300

    app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.markdown(
         color: #f8fafc !important;
     }
 
-    .stMarkdown, .stText, p, span, label, div 
+    .stMarkdown, .stText, p, span, label, div { 
         direction:rtl !important;
         text-align: right !important;
         color: #f8fafc !important;
